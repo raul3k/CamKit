778ba42187dd3ad36acf293db47ae9778866bfd8
Raul <raul.3k@gmail.com>
2014-02-28 12:03:21 -0300

    JS Updated

--- js/camkit.js
+++ js/camkit.js
@@ -1,31 +1,62 @@
-// Normalise window.URL
-window.URL || (window.URL = window.webkitURL || window.msURL || window.oURL);
+CamKit = function()
+{
+    this.selector = '';
 
-// Normalise navigator.getUserMedia
-navigator.getUserMedia || (navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-//
-// toString for the older implementation (found by https://github.com/agektmr)
-var options = {video: true, toString: function(){ return "video"; }};
-navigator.getUserMedia(options, successCallback, errorCallback);
+    this.video = true;
+    this.audio = true;
 
-// using the normalised window.URL
-function successCallback(stream) {
-    video = document.querySelector("#videoplay");
-    video.src = (window.URL && window.URL.createObjectURL) ? window.URL.createObjectURL(stream) : stream;
+    this.width = 0;
+    this.height = 0;
+
+    this.init = function() {
+        var error = function(){
+            console.log('Your browser does not support getUserMedia function. Please update it.');
+        }
+
+        // If the navigator supports the method
+        //alert(!navigator.webkitGetUserMedia);
+        if(!((navigator.getUserMedia) || (navigator.webkitGetUserMedia) || (navigator.mozGetUserMedia))){
+            error();
+            return false;
+        }
+
+
+        // Attribute missing
+        if(this.selector == '') {
+            console.log('Query selector is empty. Use CamKit.selector to select a DOM element.')
+            return false;
+        }
+
+        var selector = this.selector;
+        var width = this.width;
+        var heigth = this.height;
+
+        var attr = {video: this.video, audio: this.audio}
+
+        if(navigator.getUserMedia){
+            navigator.getUserMedia(attr, function(midia_local) {
+                video = document.querySelector(this.selector);
+                video.src = window.webkitURL.createObjectURL(midia_local);
+                video.width = width;
+                video.height = height;
+            }, error());
+        } else if(navigator.webkitGetUserMedia) {
+            navigator.webkitGetUserMedia(attr, function(midia_local) {
+                video = document.querySelector(selector);
+                video.src = window.webkitURL.createObjectURL(midia_local);
+                video.width = width;
+                video.height = height;
+            }, error());
+        } else if(navigator.mozGetUserMedia) {
+            navigator.mozGetUserMedia(attr, function(midia_local) {
+                video = document.querySelector(this.selector);
+                video.src = window.webkitURL.createObjectURL(midia_local);
+                video.width = width;
+                video.height = height;
+            }, error());
+        }
+
+        return true;
+    }
+
 }
-
-function drawFrame() {
-    var canvas = document.querySelector('canvas'),
-        context = canvas.getContext('2d');
-
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    setTimeout(drawFrame, 50);
-}
-
-ccv.detect_objects({canvas : (ccv.pre(canvas)), cascade: cascade, interval: 2, min_neighbors: 1});
-
-// highlight
-context.fillRect(face.x, face.y, face.width, face.height);
-
-// mask
-context.drawImage(mask, face.x, face.y, face.width, face.height);
